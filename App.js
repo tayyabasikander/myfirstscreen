6a7e4d99d6c065cb7904747adb9210a3e24e0476
tayyabasikander2000 <tayyaba.sikander2000@gmail.com>
2020-02-08 21:00:41 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FirstScreen from './src/FirstScreen';
+
+export default function App() {
+  return (
+    <View>
+  <FirstScreen />
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+  
+});
